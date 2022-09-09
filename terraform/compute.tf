4b3c176ab3b0dc147d007615a24c87fdd147881d
Prasoon <prasoon@example.com>
2022-09-09 10:29:57 +0000

    adding dummy

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "jenkins-instance" {
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
   description = "Allow SSH and Jenkins inbound traffic"
-  vpc_id      = "${aws_vpc.development-vpc.id}"
+  vpc_id      =                             "${aws_vpc.development-vpc.id}"
 
   ingress {
     from_port   = 22
